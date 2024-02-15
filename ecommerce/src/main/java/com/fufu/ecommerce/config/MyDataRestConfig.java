c12a325a45f702d78f84502471b0a84b96b12d21
Gregory Danquoins <danquoinsgregory@gmail.com>
2024-02-15 17:42:13 +0100

    add some rest config like expose the id

--- ecommerce/src/main/java/com/fufu/ecommerce/config/MyDataRestConfig.java
+++ ecommerce/src/main/java/com/fufu/ecommerce/config/MyDataRestConfig.java
@@ -1,5 +1,11 @@
 package com.fufu.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.HttpMethods;
@@ -10,8 +16,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import com.fufu.ecommerce.entity.Product;
 import com.fufu.ecommerce.entity.ProductCategory;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.metamodel.EntityType;
+
+
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer{
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager){
+        this.entityManager = entityManager;
+    }
     
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
@@ -31,7 +48,28 @@ public class MyDataRestConfig implements RepositoryRestConfigurer{
         .forDomainType(ProductCategory.class)
         .withItemExposure((metdata, HttpMethods)-> HttpMethods.disable(theUnsupportedActions))
         .withCollectionExposure((metdata, HttpMethods)-> HttpMethods.disable(theUnsupportedActions));
-    
+
+
+        // call an internal helper method to expose the id
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config){
+        //expose entity ids
+
+        // get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+        
+        List<Class> entityClasses = new ArrayList<>();
+
+        // get the entity types for the entities
+        for(EntityType entityType: entities){
+            entityClasses.add(entityType.getJavaType());
+        }
+
+        // expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
 
     }
 }
--- ecommerce/src/main/java/com/fufu/ecommerce/entity/Product.java
+++ ecommerce/src/main/java/com/fufu/ecommerce/entity/Product.java
@@ -40,7 +40,10 @@ public class Product {
     private String description;
 
     @Column(name = "unit_price")
+
     private BigDecimal unitPrice;
+    @Column(name = "units_in_stock")
+    private BigDecimal unitsInStock;
 
     @Column(name = "image_url")
     private String imageUrl;
